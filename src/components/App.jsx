e6e952b71bcf8cf1ac3cc5c503e083454baa5ed8
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-02-22 19:25:30 +0200

    task1,2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './Form';
+export class App extends Component {
+  state = {};
+  submitForm = data => {
+    console.log(data);
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <ContactForm onSubmit={this.submitForm} />
+        {/* React homework template */}
+      </div>
+    );
+  }
+}
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,49 @@
+const { Component } = require('react');
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  handleInput = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({ name: '', number: '' });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          namevalue={this.state.name}
+          onChange={this.handleInput}
+        />
+        <input
+          type="tel"
+          name="number"
+          // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          numbervalue={this.state.number}
+          onChange={this.handleInput}
+        />
+
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+export default ContactForm;
